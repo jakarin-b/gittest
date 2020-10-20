f565b7e6c33f4483bd5326518a53e4cd4bc432fe
Jakarin <jakarin.b@mail.kmutt.ac.th>
2020-10-20 18:05:09 +0700

    Feturev.2

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -9,10 +9,14 @@ public void getHello(){
 public void getSleep(){
 	System.out.println("Goodnight");
 }
+public void getSay(){
+	System.out.println("Fuck you");
+}
 	public static void main(String[] args) {
 		GitTest Test = new GitTest();
 		Test.getHello();
 		Test.getSleep();
+		Test.getSay();
 	}
 	
 }
