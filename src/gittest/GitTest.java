f9193085b3212fcf33cf71eeb3aa0d186b2b600f
Jakarin <jakarin.b@mail.kmutt.ac.th>
2020-10-20 15:16:28 +0700

    FirstCommit

---
+++ src/gittest/GitTest.java
@@ -0,0 +1,15 @@
+
+package gittest;
+
+
+public class GitTest {
+public void getHello(){
+			System.out.println("HelloWorld");
+		}	
+	public static void main(String[] args) {
+		GitTest Test = new GitTest();
+		Test.getHello();
+		
+	}
+	
+}
