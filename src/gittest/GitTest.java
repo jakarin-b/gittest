ea5180d0dc2d193d2f08dae1744f054e07b318e5
Jakarin <jakarin.b@mail.kmutt.ac.th>
2020-10-20 17:05:42 +0700

    newfeture

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -6,10 +6,13 @@ public class GitTest {
 public void getHello(){
 			System.out.println("HelloWorld");
 		}	
+public void getSleep(){
+	System.out.println("Goodnight");
+}
 	public static void main(String[] args) {
 		GitTest Test = new GitTest();
 		Test.getHello();
-		
+		Test.getSleep();
 	}
 	
 }
